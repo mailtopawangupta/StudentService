0bfa7a245b83a4c21b2cce41005a3aff2bcb3f53
mailtopawangupta <mailtopawangupta@gmail.com>
2020-04-15 13:28:45 +0530

    manifest.yml for pivotal cloud foundry

--- src/main/java/com/springboot/studentservice/StudentServiceApplication.java
+++ src/main/java/com/springboot/studentservice/StudentServiceApplication.java
@@ -2,7 +2,7 @@ package com.springboot.studentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 public class StudentServiceApplication {
